4c6692c81b03187e18be1d5b4fd53304c406ade8
Steven Moore <stave177@gmail.com>
2010-07-18 16:16:11 -0700

    Yet another attempt at fixing image urls...

--- lib/cijoe/server.rb
+++ lib/cijoe/server.rb
@@ -32,8 +32,7 @@ class CIJoe
       # Otherwise, render it with old_builds[0]
       erb(:statusTemplate, {},
         :joe => @joe,
-        :build => (@joe.building? ? @joe.current_build : @joe.old_builds[0]),
-        :url => CIJoe::Server.host.to_s + ":" + CIJoe::Server.port.to_s )
+        :build => (@joe.building? ? @joe.current_build : @joe.old_builds[0]) )
      
     end
 
